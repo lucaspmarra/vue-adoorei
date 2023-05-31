b5997ca55cca685e9aa1518ea8ee5a072bbc979c
lucaspmarra <marralucas1@gmail>
2023-05-31 18:43:03 -0300

    Create Login card component

---
+++ src/components/.trash.vue
@@ -0,0 +1,27 @@
+<template>
+
+    <div
+v-for="user in getUsers"
+:key="user.id">
+      {{ user.id }} - {{ user.name.firstname }} {{ user.name.lastname }}
+    </div>
+</template>
+
+<script setup>
+import { computed, onMounted } from 'vue';
+import { useHttpStore } from '../stores/http-store';
+
+const store = useHttpStore();
+
+const getUsers = computed(()=>{
+  return store.getUsers;
+});
+
+onMounted(()=>{
+  store.fetchUsers();
+});
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,24 +1,9 @@
 <script setup>
-import { computed, onMounted } from 'vue';
-import { useHttpStore } from '../stores/http-store';
-
-const store = useHttpStore();
-
-const getUsers = computed(()=>{
-  return store.getUsers;
-});
-
-onMounted(()=>{
-  store.fetchUsers();
-});
+import LoginCardVue from '../components/LoginCard.vue';
 </script>
 
 <template>
   <main>
-    <div
-v-for="user in getUsers"
-:key="user.id">
-      {{ user.id }} - {{ user.name.firstname }} {{ user.name.lastname }}
-    </div>
+<LoginCardVue/>
   </main>
 </template>
