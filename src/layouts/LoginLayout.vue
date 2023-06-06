5ac5d8177709e476f156544895b435f481539adc
Lucas Paulino Marra <lucas.marra.terceirizado@tre-go.jus.br>
2023-06-06 15:55:22 -0300

    remove testing - not using -

--- src/layouts/LoginLayout.vue
+++ src/layouts/LoginLayout.vue
@@ -1,7 +1,7 @@
 <script setup></script>
 
 <template>
-  <div class="flex flex-col justify-center min-h-screen overflow-hidden">
+  <div class="flex flex-col justify-center min-h-screen">
     <RouterView />
   </div>
 </template>
